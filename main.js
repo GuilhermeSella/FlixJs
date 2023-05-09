4e516dde7327a6b0dd523a6415cfeecdc6283ef7
Guilherme Sella Fernandes <122281956+GuilhermeSella@users.noreply.github.com>
2023-05-09 19:48:27 -0300

    update menu

---
+++ main.js
@@ -0,0 +1,12 @@
+const destaquesSemana = document.querySelector('.destaque-semana')
+
+const destaqueSeries = document.querySelector('.destaque-series')
+
+destaquesSemana.addEventListener('click', () =>{
+    location.href = 'destaquesSemana/index.html'
+    
+})
+
+destaqueSeries.addEventListener("click", ()=>{
+    location.href = 'destaquesSeries/index.html'
+})
